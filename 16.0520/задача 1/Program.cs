568ceb16bbb139337d51fe13f0bcef4f9d80679f
Yana <64301655+Alatyr2110@users.noreply.github.com>
2020-05-21 17:33:48 +0300

    Add files via upload

---
+++ 16.0520/задача 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task1
+//ТАРАНУХА ЯНА
+/*
+  Создать программу, которая будет проверять корректность ввода логина. 
+  Корректным логином будет строка от 2 до 10 символов, 
+  содержащая только буквы латинского алфавита или цифры, 
+  при этом цифра не может быть первой:
+а) без использования регулярных выражений; 
+*/
+{
+    class Program
+    {
+        public static bool LoginInput(string s)//
+        {
+            if (s.Length < 2 || s.Length > 10) 
+            return false;
+
+            if (Char.IsDigit(s[0]))//честно содрала из учебника
+                return false;
+
+            for (int i = 1; i < s.Length; i++)
+                if (!(Char.IsLetterOrDigit(s[i]))) //это тоже
+                    return false;
+            return true;
+        }
+
+        static void Main(string[]args)
+        {
+            
+            string login = Console.ReadLine();
+            
+            if (LoginInput(login)) 
+                Console.WriteLine("Проверку прошли");
+            else 
+                Console.WriteLine("Прoверку не прошли");
+         
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ 16.0520/задача 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+//ТАРАНУХА ЯНА
+/*
+Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+а) Вывести только те слова сообщения,  которые содержат не более n букв. 4 буквы
+б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+в) Найти самое длинное слово сообщения.
+г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+ */
+{
+    class Program
+    {
+        static void WordLeng(string s, int len)
+        {
+           string[] ss = s.Split(' ');
+            foreach (string item in ss)
+                if (item.Length == len)
+                    Console.WriteLine(item);
+                    //return;
+        }
+        static string WordDelLet(string s)
+        {
+            string[] ss = s.Split(' ');
+            string newStr = "";
+            foreach (string num in ss) //var - string? ругается; все равно ругается
+                if (num[num.Length - 1] != 'ь')
+                    if (num[num.Length - 1] != ',')
+                        newStr += num + ' ';
+           // Console.WriteLine(newStr);
+            return newStr;
+        }
+        static int WordMax(string s)
+        {
+            int max = 0;
+            string[] ss = s.Split(' ');
+            foreach (string num in ss)
+                if (max < num.Length) //здесь вообще ругается
+                    max = num.Length; // минус запятая
+            //Console.WriteLine(ss);
+            //Console.WriteLine(max);
+            return max;
+
+        }
+             static void Main(string[]args)
+        {
+            string s = "ноль , один , два , три , четыре , пять , шесть , семь , восемь , девять , десять , квакваква";
+            Console.WriteLine(s);
+
+            Console.WriteLine("Слова из 5 букв:"); WordLeng(s, 5); //как так???
+            //Console.WriteLine(WordLeng(s, 5));
+
+            Console.WriteLine("Слова без 'ь':");
+            Console.WriteLine(WordDelLet(s));
+
+            Console.WriteLine("Самое длинное слово состоит из  " + WordMax(s) + "  букв");
+           // Console.WriteLine(WordMax(s));
+            
+            Console.ReadKey();
+        }
+        
+
+        
+    }
+}
